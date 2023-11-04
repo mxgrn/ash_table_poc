2a44e27f01d807ba76cd838adff0cf852d7af557
Max Gorin <regzok@gmail.com>
2023-11-04 13:53:58 +0200

    More seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -15,7 +15,14 @@
   %{title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925},
   %{title: "The Grapes of Wrath", author: "John Steinbeck", year: 1939},
   %{title: "Nineteen Eighty-Four", author: "George Orwell", year: 1949},
-  %{title: "Ulysses", author: "James Joyce", year: 1922}
+  %{title: "Ulysses", author: "James Joyce", year: 1922},
+  %{title: "Lolita", author: "Vladimir Nabokov", year: 1955},
+  %{title: "Catch-22", author: "Joseph Heller", year: 1961},
+  %{title: "The Catcher in the Rye", author: "J. D. Salinger", year: 1951},
+  %{title: "Beloved", author: "Toni Morrison", year: 1987},
+  %{title: "The Sound and the Fury", author: "William Faulkner", year: 1929},
+  %{title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960},
+  %{title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954},
 ]
 |> Enum.map(fn book ->
   AshTable.Book
